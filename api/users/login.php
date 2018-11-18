f0ef1447f90d0f0d3437114f4f53a87d287e4144
jeanpierobom <pierobom@gmail.com>
2018-11-18 15:34:04 -0800

    API updated

--- api/users/login.php
+++ api/users/login.php
@@ -1,13 +1,23 @@
 <?php
-header('Access-Control-Allow-Origin: *');
 header('Content-Type: application/json');
-header("Access-Control-Allow-Methods: POST");
-header("Access-Control-Allow-Headers: Access-Control-Allow-Headers,Content-Type,Access-Control-Allow-Methods, Authorization, X-Requested-With");
 
-require_once "../../config/Database.php";
-require_once "../../models/HttpResponse.php";
-require_once "../../models/User.php";
-require_once "../../models/Authenticate.php";
+// Allow from any origin
+if (isset($_SERVER['HTTP_ORIGIN'])) {
+  header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
+  header('Access-Control-Allow-Credentials: true');
+  header('Access-Control-Max-Age: 86400'); // cache for 1 day
+}
+
+// Access-Control headers are received during OPTIONS requests
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+  if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
+    header("Access-Control-Allow-Methods: GET, POST, PUT, OPTIONS");
+
+  if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
+    header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
+
+  exit(0);
+}
 
 $db = new Database();
 $http = new HttpResponse();
